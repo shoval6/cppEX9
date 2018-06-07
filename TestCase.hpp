009df5a1efe48a47416321ffe01bfdd43fb3167e
shoval6 <shovalhm@gmail.com>
2018-06-07 19:14:34 +0300

    fix bugs

--- TestCase.hpp
+++ TestCase.hpp
@@ -19,26 +19,24 @@ public:
 
     TestCase(string testName, ostream &os) : testName(testName), os(os),passed(0) , failed(0) {};
 
-    template<class T>
+    template<typename T>
     TestCase& check_equal(T x, T y);
 
-    template<class T>
+    template<typename T>
     TestCase& check_different(T x, T y);
 
-    template<class T , class R>
-    TestCase& check_function(R(*func)(T s), T x, R y);
+    template<typename R , typename T , typename S>
+    TestCase& check_function(R func, T x, S y);
 
-    template <class T>
+    template <typename T>
     TestCase& check_output(T x , string str);
 
     void print();
 
 
-
-
 };
 
-template<class T>
+template<typename T>
 TestCase &TestCase::check_equal(T x, T y) {
     if (x == y)
         passed++;
@@ -54,7 +52,7 @@ TestCase &TestCase::check_equal(T x, T y) {
     return *this;
 }
 
-template<class T>
+template<typename T>
 TestCase& TestCase::check_different(T x, T y) {
     if (x != y)
         passed++;
@@ -66,34 +64,40 @@ TestCase& TestCase::check_different(T x, T y) {
         string msg = save1.str() + " should'nt equal " + save2.str() + "!";
         write2stream(msg);    }
 
+        return *this;
 }
 
-template <class T , class R>
-TestCase& TestCase::check_function(R (*func)(T), T x, R y) {
-    T temp = (*func)(x);
+template <typename R , typename T , typename S>
+TestCase& TestCase::check_function(R func , T x, S y) {
+    T temp = func(x);
     if(temp == y)
         passed++;
     else{
         failed++;
         ostringstream save1, save2;
-        save1 << x;
+        save1 << temp;
         save2 << y;
-        string msg = "Function should return " + save1.str() + " but returned " + save2.str() + "!";;
+        string msg = "Function should return " + save2.str() + " but returned " + save1.str() + "!";;
         write2stream(msg);
     }
-
     return *this;
 }
 
 
-template <class T>
+template <typename T>
 TestCase& TestCase::check_output(T x, string str) {
-    ostringstream save;
-    save << x;
-    if (save.str() == str)
+    ostringstream out;
+    out << x;
+    if (out.str() == str)
+    {
         passed++;
-    else
+    }
+    else{
         failed++;
+        string msg = "string value should be " + str + " but is " + out.str();
+        write2stream(msg);
+
+    }
+
+    return (*this);
 }
-
-
